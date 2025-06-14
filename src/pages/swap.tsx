72151c0b95f90ad87b0bd7d7c6efc989a44b6b65
linzeguang <15817289730@163.com>
2025-06-14 14:53:16 +0800

    remove style

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
-import SwapForm from '@/components/swap/SwapForm'
-
 const Swap: React.FC = () => {
   return (
     <div className="mx-auto w-[558px]">
       <appkit-button />
-      <SwapForm />
     </div>
   )
 }
